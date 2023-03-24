0ca24be8cac3fa3ebd2333fd0c42392e64c1fc64
amankumar1988 <127178180+amankumar1988@users.noreply.github.com>
2023-03-24 12:18:12 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+
+if [ $(id -u) -ne 0 ]; then 
+    echo -e "You should perform this as root user"
+    exit 1
+fi 
+Stat() {
+    if [ $1 -ne 0 ]; then 
+        echo "Installation Failed :  Check log /tmp/jinstall.log"
+        exit 2 
+    fi 
+}
+
+yum install fontconfig java-11-openjdk-devel wget -y  &>/tmp/jinstall.log
+Stat $?
+wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo &>>/tmp/jinstall.log 
+Stat $?
+
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key &>>/tmp/jinstall.log 
+Stat $?
+
+yum install jenkins -y &>>/tmp/jinstall.log
+Stat $?
+
+systemctl enable jenkins  &>>/tmp/jinstall.log 
+Stat $?
+
+systemctl start jenkins  &>>/tmp/jinstall.log 
+Stat $?
+
+echo -e "\e[32m INSTALLATION SUCCESSFUL\e[0m"
+
+mkdir -p /var/lib/jenkins/.ssh
+echo 'Host *
+    UserKnownHostsFile /dev/null
+    StrictHostKeyChecking no' >/var/lib/jenkins/.ssh/config
+chown jenkins:jenkins /var/lib/jenkins/.ssh -R
+chmod 400 /var/lib/jenkins/.ssh/config
